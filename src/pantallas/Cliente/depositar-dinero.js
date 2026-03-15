567753f4846410417460a970176572b7041785ed
Jaziel Vargas Choque <jazielarmandovargaschoque@gmail.com>
2026-03-14 23:23:46 -0400

    Modificamos la pantalla depositar dinero

--- src/pantallas/Cliente/depositar-dinero.js
+++ src/pantallas/Cliente/depositar-dinero.js
@@ -5,9 +5,12 @@ import ImportarNav from "../../Importar nav/importar-nav";
 function DepositarDinero() {
     
     return (
-        <div className="Depositar">
+        <div className="contenedor">
             <ImportarNav />
-            <h1>depositar dinero</h1>
+            <h1 className="Depositar">depositar dinero</h1>
+            {/**mostrar la cantidad a depositar, si quiere comprar dolares mostrar al tipo de cambio paralelo y oficial en bolivia
+             * , mostar la tasa de cambio neta 
+             */}
         </div>
     )
 };
